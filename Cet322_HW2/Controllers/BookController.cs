adf8d66374cd4a673925d567bd226fcc4cb976f2
Monster <Monster@DESKTOP-V3KNKVD>
2024-03-05 23:07:43 +0300

    Add project files.

---
+++ Cet322_HW2/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using Cet322_HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Cet322_HW1.Controllers
+{
+    public class BookController : Controller
+    {
+        static List<Book> books = new List<Book>();
+        public IActionResult Display()
+        {
+            // Kitap listesini görüntüleme sayfasına gönder
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }  
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("BookName,Author,PageNumber,DueDate")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                // Kitabı listeye ekle
+                books.Add(book);
+
+                // Kitap listesini gösteren Display sayfasına yönlendir
+                return RedirectToAction(nameof(Display));
+            }
+            return View(book);
+        }
+    }
+}
---
+++ Cet322_HW2/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Cet322_HW1.Models
+{
+    public class Book
+    {
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
